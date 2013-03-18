cf1e3ce02a7b8ea5e8afd87e706845466991d9f0
Std <adam@localhost.localdomain>
2013-03-18 20:02:15 +0100

    z1

--- 03/z1.cpp
+++ 03/z1.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 #define L1 1.0
 #define L2 0.001
+#define KROK_MAX 1e6
 
 double func1(double t, double y1, double y2)
 {
@@ -46,7 +47,7 @@ void rk2_uklad(double& u1, double& u2, double prevu1, double prevu2, double prev
     u2 = prevu2 + (dt/2.0)*(k12 + k22);
 }
 
-double dokladne(double& y1, double& y2, double t)
+void dokladne(double& y1, double& y2, double t)
 {
     y1 = 0.5*((L1+L2)/(L2-L1))*exp(-L1*t)+L2/(L1-L2)*exp(-L2*t)+1./2.;
     y2 = exp(-L2*t);
@@ -62,7 +63,7 @@ double new_time(double S, double tol, double E, double dt, double n)
     return pow((S*tol)/abs(E), 1.0/n)*dt;
 }
 
-void zad3(double y11, double y21, double S, double T, double tol,
+void zad1_1(double n, double y11, double y21, double S, double T, double tol,
           void (*f)(double&, double&, double, double, double, double, double (*f1)(double, double, double), double (*f2)(double, double, double)),
           char* file)
 {
@@ -104,9 +105,59 @@ void zad3(double y11, double y21, double S, double T, double tol,
     cout << krok << endl;
 }
 
+void zad1_2(double n, double y11, double y21, double S, double T, double tol,
+          void (*f)(double&, double&, double, double, double, double, double (*f1)(double, double, double), double (*f2)(double, double, double)),
+          char* file)
+{
+    ofstream out(file);
+    double dt = 0.1;
+    double y12, y22;
+    double prevy1, prevy2;
+    int krok = 0;
+    for(double t = dt; t < T;){
+        prevy1 = y11;
+        prevy2 = y12;
+        y12 = y11;
+        y22 = y21;
+
+        f(y11, y21, y11, y21, t, dt, func1, func2);
+
+        f(y12, y22, y12, y22, t, dt/2.0, func1, func2);
+        f(y12, y22, y12, y22, t + dt/2.0, dt/2.0, func1, func2);
+
+        double Ev = err(y11, y12, 5);
+        double Ex = err(y21, y22, 5);
+
+        if(Ex < tol && Ev < tol)
+        {
+            double y1, y2;
+            dokladne(y1, y2, t);
+            out << t << " " << dt << " " << y12 << " " << y22 << " " << y1 << " " << y2;
+            t+=dt;
+        }
+
+        double vtime = new_time(S, tol, Ev, dt, 5);
+        double xtime = new_time(S, tol, Ex, dt, 5);
+        out << " " << vtime << " " << xtime << " \n";
+        dt = vtime > xtime ? xtime : vtime;
+        ++krok;
+        if(t > 5000)
+        {
+            dt = 100;
+        }
+        if(krok > KROK_MAX) {
+            cout << "niestabilne! ";
+            break;
+        }
+        //cout << dt << endl;
+    }
+    cout << krok << endl;
+}
+
 
 int main()
 {
-    zad3(0, 1, 0.75, 10000, 0.00001, rk4_uklad, "z3.txt");
-    zad3(0, 1, 0.75, 10000, 0.00001, rk2_uklad, "z3_rk2.txt");
+    zad1_1(5, 0, 1, 0.75, 10000, 0.00001, rk4_uklad, "z1.txt");
+    zad1_1(3, 0, 1, 0.75, 10000, 0.00001, rk2_uklad, "z1_rk2.txt");
+    zad1_2(3, 0, 1, 0.75, 10000, 0.00001, rk2_uklad, "z1_1.txt");
 }
