eea0be045c8dfdd598eb4664e242a3d34f4c6350
Adam <adam.syrek@gmail.com>
2013-03-27 12:31:30 +0100

    04

--- 04/03.cpp
+++ 04/03.cpp
@@ -34,55 +34,121 @@ double f2(double t, double u1, double u2)
 	return -L2 * u2;
 }
 
+double err(double y2, double y1, double n)
+{
+    return (y2-y1)/(pow(2.0,n-1.0) - 1.0);
+}
+
+double new_time(double S, double tol, double E, double dt, double n)
+{
+    return pow((S*tol)/abs(E), 1.0/n)*dt;
+}
+
 void niejawneRK(double u01, double u02, double tol, double dt, double T, string filename, bool print_first_step = false)
 {
-	double u1 = u01, u2 = u02;
-	double U1_1,U2_1, U1_2, U2_2;
-
-	int iter;
-	double err;
-
+	double u1_krotki, u2_krotki, u1_dlugi, u2_dlugi;
+    u1_krotki = u1_dlugi = u01;
+    u2_krotki = u2_dlugi = u02;
+	double U1_1_krotki, U1_2_krotki, U2_1_krotki, U2_2_krotki, U1_1_dlugi, U1_2_dlugi, U2_1_dlugi, U2_2_dlugi;
+    double prevu1 = u01, prevu2 = u02;
+    double tmpdt;
+			U1_1_krotki = U1_2_krotki = U1_1_dlugi = U1_2_dlugi = prevu1;
+		U2_1_krotki = U2_2_krotki = U2_1_dlugi = U2_2_dlugi = prevu2;
 	float **A = matrix(1,4,1,4);
     float **B = matrix(1,4,1,1);
 	ofstream file;
 	if(!filename.empty())
 		file.open(filename.c_str());
 
-	for(double t = 0; t <= T ; t += dt)
+	for(double t = 0; t <= T ;)
 	{
-		U1_1 = U1_2 = u1;
-		U2_1 = U2_2 = u2;
 
+        //dlugi?
+        tmpdt = dt * 2;
+        
+		A[1][1] = 1 + L1 * a[0][0] * tmpdt;	A[1][2] = -L2 * a[0][0] * tmpdt;	A[1][3] = L1 * a[0][1] * tmpdt;	A[1][4] = -L2 * a[0][1] * tmpdt;
+		A[2][1] = L1 * a[1][0] * tmpdt;		A[2][2] = -L2 * a[1][0] * tmpdt;	A[2][3] = 1 + L1 * a[1][1] * tmpdt;A[2][4] = -L2 * a[1][1] * tmpdt;
+		A[3][1] = 0;						A[3][2] = 1 + L2 * a[0][0] * tmpdt;A[3][3] = 0;					A[3][4] = L2 * a[0][1] * tmpdt;
+		A[4][1] = 0;						A[4][2] = L2 * a[1][0] * tmpdt;	A[4][3] = 0;					A[4][4] = 1 + L2 * a[1][1] * tmpdt;
 			
-			A[1][1] = 1 + L1 * a[0][0] * dt;	A[1][2] = -L2 * a[0][0] * dt;	A[1][3] = L1 * a[0][1] * dt;	A[1][4] = -L2 * a[0][1] * dt;
-			A[2][1] = L1 * a[1][0] * dt;		A[2][2] = -L2 * a[1][0] * dt;	A[2][3] = 1 + L1 * a[1][1] * dt;A[2][4] = -L2 * a[1][1] * dt;
-			A[3][1] = 0;						A[3][2] = 1 + L2 * a[0][0] * dt;A[3][3] = 0;					A[3][4] = L2 * a[0][1] * dt;
-			A[4][1] = 0;						A[4][2] = L2 * a[1][0] * dt;	A[4][3] = 0;					A[4][4] = 1 + L2 * a[1][1] * dt;
+		B[1][1] = u1_dlugi + (tmpdt * L1) * (a[0][0] + a[0][1]) / 2.;
+		B[2][1] = u1_dlugi + (tmpdt * L1) * (a[1][0] + a[1][1]) / 2.;
+		B[3][1] = u2_dlugi;
+		B[4][1] = u2_dlugi;
 			
-			B[1][1] = u1 + (dt * L1) * (a[0][0] + a[0][1]) / 2.;
-			B[2][1] = u1 + (dt * L1) * (a[1][0] + a[1][1]) / 2.;
-			B[3][1] = u2;
-			B[4][1] = u2;
-			
-			gaussj(A,4,B,1);
-			
-			err = max(fabs(B[1][1]), max(fabs(B[2][1]), max(fabs(B[3][1]), fabs(B[4][1]))));
-			
-			U1_1 = U1_1 + B[1][1];
-			U1_2 = U1_2 + B[3][1];
-			U2_1 = U2_1 + B[2][1];
-			U2_2 = U2_2 + B[4][1];
-			cout << err << endl;
+		gaussj(A,4,B,1);
+					
+		U1_1_dlugi = U1_1_dlugi + B[1][1];
+		U1_2_dlugi = U1_2_dlugi + B[3][1];
+		U2_1_dlugi = U2_1_dlugi + B[2][1];
+		U2_2_dlugi = U2_2_dlugi + B[4][1];
 
-		u1 = u1 + (dt / 2.) * (f1(t, U1_1, U2_1) + f1(t, U1_2, U2_2));
-		u2 = u2 + (dt / 2.) * (f2(t, U1_1, U2_1) + f2(t, U1_2, U2_2));
-		if(!filename.empty())
-			file << t << " " << u1 << " " << u2 << endl;
+		u1_dlugi = u1_dlugi + (dt / 2.) * (f1(t, U1_1_dlugi, U2_1_dlugi) + f1(t, U1_2_dlugi, U2_2_dlugi));
+		u2_dlugi = u2_dlugi + (dt / 2.) * (f2(t, U1_1_dlugi, U2_1_dlugi) + f2(t, U1_2_dlugi, U2_2_dlugi));
+		
+        //2 krotkie?     
+		A[1][1] = 1 + L1 * a[0][0] * dt;	A[1][2] = -L2 * a[0][0] * dt;	A[1][3] = L1 * a[0][1] * dt;	A[1][4] = -L2 * a[0][1] * dt;
+		A[2][1] = L1 * a[1][0] * dt;		A[2][2] = -L2 * a[1][0] * dt;	A[2][3] = 1 + L1 * a[1][1] * dt;A[2][4] = -L2 * a[1][1] * dt;
+		A[3][1] = 0;						A[3][2] = 1 + L2 * a[0][0] * dt;A[3][3] = 0;					A[3][4] = L2 * a[0][1] * dt;
+		A[4][1] = 0;						A[4][2] = L2 * a[1][0] * dt;	A[4][3] = 0;					A[4][4] = 1 + L2 * a[1][1] * dt;
+			
+		B[1][1] = u1_krotki + (dt * L1) * (a[0][0] + a[0][1]) / 2.;
+		B[2][1] = u1_krotki + (dt * L1) * (a[1][0] + a[1][1]) / 2.;
+		B[3][1] = u2_krotki;
+		B[4][1] = u2_krotki;
+			
+		gaussj(A,4,B,1);
+					
+		U1_1_krotki = U1_1_krotki + B[1][1];
+		U1_2_krotki = U1_2_krotki + B[3][1];
+		U2_1_krotki = U2_1_krotki + B[2][1];
+		U2_2_krotki = U2_2_krotki + B[4][1];
+
+		u1_krotki = u1_krotki + (dt / 2.) * (f1(t, U1_1_krotki, U2_1_krotki) + f1(t, U1_2_krotki, U2_2_krotki));
+		u2_krotki = u2_krotki + (dt / 2.) * (f2(t, U1_1_krotki, U2_1_krotki) + f2(t, U1_2_krotki, U2_2_krotki));
+        
+        A[1][1] = 1 + L1 * a[0][0] * dt;	A[1][2] = -L2 * a[0][0] * dt;	A[1][3] = L1 * a[0][1] * dt;	A[1][4] = -L2 * a[0][1] * dt;
+		A[2][1] = L1 * a[1][0] * dt;		A[2][2] = -L2 * a[1][0] * dt;	A[2][3] = 1 + L1 * a[1][1] * dt;A[2][4] = -L2 * a[1][1] * dt;
+		A[3][1] = 0;						A[3][2] = 1 + L2 * a[0][0] * dt;A[3][3] = 0;					A[3][4] = L2 * a[0][1] * dt;
+		A[4][1] = 0;						A[4][2] = L2 * a[1][0] * dt;	A[4][3] = 0;					A[4][4] = 1 + L2 * a[1][1] * dt;
+			
+		B[1][1] = u1_krotki + (dt * L1) * (a[0][0] + a[0][1]) / 2.;
+		B[2][1] = u1_krotki + (dt * L1) * (a[1][0] + a[1][1]) / 2.;
+		B[3][1] = u2_krotki;
+		B[4][1] = u2_krotki;
+			
+		gaussj(A,4,B,1);
+			
+		U1_1_krotki = U1_1_krotki + B[1][1];
+		U1_2_krotki = U1_2_krotki + B[3][1];
+		U2_1_krotki = U2_1_krotki + B[2][1];
+		U2_2_krotki = U2_2_krotki + B[4][1];
+
+		u1_krotki = u1_krotki + (dt / 2.) * (f1(t, U1_1_krotki, U2_1_krotki) + f1(t, U1_2_krotki, U2_2_krotki));
+		u2_krotki = u2_krotki + (dt / 2.) * (f2(t, U1_1_krotki, U2_1_krotki) + f2(t, U1_2_krotki, U2_2_krotki));
+        
+        double errU1 = fabs(err(u1_dlugi, u1_krotki, 5));
+        double errU2 = fabs(err(u2_dlugi, u2_krotki, 5));
+        
+        if(errU1 < tol && errU2 < tol)
+        {
+            prevu1 = u1_krotki;
+            prevu2 = u2_krotki;
+            t+= 2* dt;
+            file << t << " " << prevu1 << " " << prevu2 << " " << dt << endl;
+        }
+        
+        double dtU1 = new_time(0.75, tol, errU1, dt, 5);
+        double dtU2 = new_time(0.75, tol, errU2, dt, 5);
+        
+        dt = min(dtU1, dtU2);
+        
+        cout << dt << endl;
 
 	}
 
-    free_matrix(A,1,2,1,2);
-    free_matrix(B,1,2,1,1);
+    free_matrix(A,1,4,1,4);
+    free_matrix(B,1,4,1,1);
 }
 
 int main()
